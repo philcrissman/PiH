ffa88404fd72c6d967fa5f2caf9537a49221f51a
Phil Crissman <phil.crissman@gmail.com>
2026-01-05 23:19:41 -0800

    ch3 done

--- ch3.hs
+++ ch3.hs
@@ -39,3 +39,16 @@ second xs = head (tail xs)
 
 swap :: (a,b) -> (b,a)
 swap (x,y) = (y,x)
+
+pair :: a -> b -> (a, b)
+pair x y = (x,y)
+
+double :: Num a => a -> a
+double x = x * 2
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (t -> t) -> t -> t
+twice f x = f (f x)
+
